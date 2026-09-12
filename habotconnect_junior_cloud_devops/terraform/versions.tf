439a4f2ab54433d1e4678779ce10b7bf19002707
Ramkrishna <rupambarmanxyz@gmail.com>
2026-09-12 23:24:55 +0530

    terraform versions file

---
+++ habotconnect_junior_cloud_devops/terraform/versions.tf
@@ -0,0 +1,16 @@
+
+terraform {
+  required_version = ">= 1.6.0"
+
+  required_providers {
+    google = {
+      source  = "hashicorp/google"
+      version = ">= 7.45.0, < 8.0.0"
+    }
+  }
+}
+
+provider "google" {
+  project = var.project_id
+  region  = var.region
+}
